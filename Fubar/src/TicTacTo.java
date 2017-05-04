c9f217da36b0bcdc4e350b27c3d0d8a27bc11709
waford <waford@Nebula>
2017-05-03 22:24:33 -0400

    First Commit 5/3/17

---
+++ Fubar/src/TicTacTo.java
@@ -0,0 +1,139 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacTo {
+
+	static String[] position;
+
+	public static void main(String[] args) {
+		startUp();
+	}
+
+	private static void startUp() {
+		initialize();
+		play();
+
+	}
+
+	private static void initialize() {
+		position = new String[9];
+		for (int i = 0; i <= 8; i++) {
+			position[i] = Integer.toString(i + 1);
+		}
+
+		System.out.println(
+				"Thank you for playing Tic Tack Toe. The board setup is below. You are X's, and the computer is O's. \n");
+		drawBoard();
+		for (int i = 0; i <= 8; i++) {
+			position[i] = " ";
+		}
+
+	}
+
+	private static void drawBoard() { // sets up board
+		System.out.println("_" + position[0] + "_|_" + position[1] + "_|_" + position[2] + "_" + "\n_" + position[3]
+				+ "_|_" + position[4] + "_|_" + position[5] + "_" + "\n_" + position[6] + "_|_" + position[7] + "_|_"
+				+ position[8] + "_" + "\n");
+
+	}
+
+	private static void play() {
+		Scanner in = new Scanner(System.in);
+		for (int i = 0; i < 5; i++) {
+			if (i == 4) {
+				getMove(in);
+				drawBoard();
+				checkForWin();
+				return;
+			} else {
+				getMove(in);
+				compMove();
+				drawBoard();
+				if (checkForWin()) {
+					break;
+				}
+			}
+
+		}
+		in.close();
+	}
+
+	private static boolean checkForWin() { //sets up and checks for win conditions using a 2-d array
+		int oCount;
+		int xCount;
+		String[][] tmp = new String[3][3];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				tmp[i][j] = position[3 * i + j];
+			}
+		}
+
+		if (checkRows(tmp) || checkCol(tmp) || checkDiag(tmp)) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	private static boolean checkRows(String[][] tmp) {
+		int xCount, yCount;
+		for(int i = 0; i < 3; i++) {
+			xCount = 0; 
+			yCount = 0;
+			for(String s : tmp[i]){
+				if(s.equals("O")){
+					yCount++;
+				} else if(s.equals("X")){
+					xCount++;
+				}				
+			}
+			if(yCount) //need to change checkWin etc to return 0,1,2 to determin if no win, x win or y win. 
+		}
+
+	return false;
+
+	}
+
+	private static boolean checkCol(String[][] tmp) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private static boolean checkDiag(String[][] tmp) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private static void getMove(Scanner in) { // gets the move, and inputs an
+												// "x" into the selected
+												// position array
+
+		try {
+			System.out.print("Please input position for move:");
+			String move = in.next();
+			int moveInt = Integer.parseInt(move);
+
+			if (1 > moveInt && moveInt > 9 || position[moveInt - 1].equals("O")) {
+				throw new NumberFormatException();
+			}
+			position[moveInt - 1] = "X";
+			System.out.println("\n");
+			return;
+		} catch (NumberFormatException e) {
+			System.out.println("Please input a valid integer between 1 and 9, inclusive \n");
+			getMove(in);
+		}
+	}
+
+	private static void compMove() {
+		Random random = new Random();
+		int move = random.nextInt(8);
+		while (position[move].equals("X") || position[move].equals("O")) {
+			move = random.nextInt(8);
+		}
+		position[move] = "0";
+		return;
+	}
+}
